298ab357f76f4aa41997d933398c67066cffede3
Tom Roar Furunes <tom.roar@halloverden.no>
2023-04-27 15:57:45 +0200

    ADD: base bundle setup

---
+++ src/FagforbundetNotificationApiSmsNotifierBundle.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Fagforbundet\NotificationApiSmsNotifierBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FagforbundetNotificationApiSmsNotifierBundle extends Bundle {
+}
