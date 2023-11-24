d95ae3c9e7b7900093b861a1e2a7ad399f6dbd77
Tom Roar Furunes <tom.roar@halloverden.no>
2023-11-24 12:01:45 +0100

    EDIT: update bundle setup

--- src/FagforbundetNotificationApiSmsNotifierBundle.php
+++ src/FagforbundetNotificationApiSmsNotifierBundle.php
@@ -2,7 +2,29 @@
 
 namespace Fagforbundet\NotificationApiSmsNotifierBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Fagforbundet\NotificationApiSmsNotifierBundle\Transport\NotificationApiTransportFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
-class FagforbundetNotificationApiSmsNotifierBundle extends Bundle {
+class FagforbundetNotificationApiSmsNotifierBundle extends AbstractBundle {
+
+  /**
+   * @inheritDoc
+   */
+  public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void {
+    $alias = $this->getContainerExtension()->getAlias();
+
+    $container->services()
+      ->set($alias. '.transport_factory', NotificationApiTransportFactory::class)
+      ->args([
+        service('fagforbundet_notification_api_client.client'),
+        service('event_dispatcher')->nullOnInvalid(),
+        service('http_client')->nullOnInvalid()
+      ])
+      ->tag('texter.transport_factory')
+    ;
+  }
+
 }
